7ff622efef97a9ae1e8e82e514cfcba9cbdb64a6
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-02-21 19:51:59 +0530

    feat: added responsive navbar in other components

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const respSidebar = document.querySelector('.respSidebar')
+
+const toggleHam = () => {
+  respSidebar.style.display = 'flex'
+}
+
+const closeNav = () => {
+  respSidebar.style.display = 'none'
+}
